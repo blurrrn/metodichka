548fbfb6f8153f1641107854090d195f65b60754
blurrrn <103361796+blurrrn@users.noreply.github.com>
2022-12-02 13:31:13 +0400

    Add files via upload

---
+++ 5-I 12.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    double x, s=0;
+    cout << "n=\n";
+    cin >> n;
+    cout << "x=\n";
+    cin >> x;
+    for (int i = 1; i <= n; i++) {
+        s += sin(pow(x, i));
+    }
+    cout << "s=" << s;
+}
