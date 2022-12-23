abbcae84753f17df4162e0674b2c70190d32d23a
blurrrn <103361796+blurrrn@users.noreply.github.com>
2022-12-23 13:02:52 +0400

    Add files via upload

---
+++ 7-II-14.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string s, s0, res; int l;
+    char c;
+    cout << "Введите строки S, S0 и символ С:\n";
+    cin >> s >> s0 >> c;
+    string::size_type len = s.size();
+    l = s.size();
+    for (int i = 0; i < l ; i++) {
+        res += s[i];
+        if (s[i] == c) {
+            res+=s0;
+        }
+    }
+    cout << res;
+}
+
