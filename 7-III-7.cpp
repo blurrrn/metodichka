3ee0180a2e8d5e1004a678d01301f7affa0ffb7a
blurrrn <103361796+blurrrn@users.noreply.github.com>
2022-12-23 14:08:48 +0400

    Add files via upload

---
+++ 7-III-7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string s, slovo;
+    char c, C;
+    string::size_type k = 0, pos = 0;
+    string r = ",.:;!? ";
+    cout << "Введите предложение S и символ С:\n";
+    getline(cin, s);
+    cin >> c >> C;
+    k = s.find_first_of(r, pos);
+    while (k!=s.npos){
+        slovo = s.substr(pos, k - pos);
+        while (ispunct(s[k]))k++;
+        pos = k + 1;
+        if (slovo[0] == c || slovo[0] == C) cout << slovo << endl;
+        k = s.find_first_of(r, pos);
+    }
+}
